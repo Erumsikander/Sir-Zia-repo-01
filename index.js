b35c7568ce1514c0b43fa49e5ab211fe7f2a91e6
Erum Sikander <mrsikanderghouri@gmail.com>
2024-05-21 08:16:29 +0500

    new adding

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 "use strict";
+// Inferring types =  The TypeScript compiler can infer the type of x based on the expressions that are used to initialize it
 Object.defineProperty(exports, "__esModule", { value: true });
 console.log("Hello Erum Sikander");
+let obj = {
+    name: 'Erum Sikander',
+    age: 24,
+    city: 'Hyderabad',
+    developer: true,
+};
+console.log(obj);
+// uses ---- JSon Stand for Javascript Object Notation
+console.log(JSON.stringify(obj));
--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
+
+// Inferring types =  The TypeScript compiler can infer the type of x based on the expressions that are used to initialize it
+
 console.log("Hello Erum Sikander");
+let obj = {
+name: 'Erum Sikander',
+age : 24,
+city : 'Hyderabad', 
+developer : true,
+}
+console.log(obj);
+// uses ---- JSon Stand for Javascript Object Notation
+console.log(JSON.stringify(obj));
