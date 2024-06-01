dd862fd15ae39113ccecac9c21878acb69172dfb
Erum Sikander <mrsikanderghouri@gmail.com>
2024-06-02 04:10:58 +0500

    complete Sir zia repo

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 
+
+
 // Inferring types =  The TypeScript compiler can infer the type of x based on the expressions that are used to initialize it
 
 console.log("Hello Erum Sikander");
@@ -9,5 +11,6 @@ city : 'Hyderabad',
 developer : true,
 }
 console.log(obj);
-// uses ---- JSon Stand for Javascript Object Notation
+// uses ---- JSon Stand for Javascript Object Notation and json is a text-based data format that is used to store and transfer data:
+
 console.log(JSON.stringify(obj));
